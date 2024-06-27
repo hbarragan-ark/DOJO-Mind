95cd5bc2d525c796aa6cedd902b326e02e33e6e8
Hector Herrera <hbarragan@arkusnexus.com>
2024-06-27 13:44:51 -0700

    Change kata #2 constants

--- Kata-2/RomanNumerals.rb
+++ Kata-2/RomanNumerals.rb
@@ -1,17 +1,22 @@
+ROMAN_NUMERALS = {
+  1000 => "M", 900 => "CM", 500 => "D", 400 => "CD",
+  100 => "C", 90 => "XC", 50 => "L", 40 => "XL",
+  10 => "X", 9 => "IX", 5 => "V", 4 => "IV", 1 => "I"
+}
+
+INTEGER_NUMERALS = {
+  "I" => 1, "V" => 5, "X" => 10, "L" => 50,
+  "C" => 100, "D" => 500, "M" => 1000
+}
+
 def num_in_range?(num)
   num > 0 && num < 4000
 end
 
 def decimal_to_roman(num)
-  roman_numerals = {
-    1000 => "M", 900 => "CM", 500 => "D", 400 => "CD",
-    100 => "C", 90 => "XC", 50 => "L", 40 => "XL",
-    10 => "X", 9 => "IX", 5 => "V", 4 => "IV", 1 => "I"
-  }
-
   roman = ""
 
-  roman_numerals.each do |value, letter|
+  ROMAN_NUMERALS.each do |value, letter|
     break if num == 0
 
     next if num < value
@@ -28,16 +33,11 @@ def valid_roman_characters?(roman)
 end
 
 def roman_to_decimal(roman)
-  values = {
-    "I" => 1, "V" => 5, "X" => 10, "L" => 50,
-    "C" => 100, "D" => 500, "M" => 1000
-  }
-
   total = 0
   prev_value = 0
 
   roman.each_char do |char|
-    value = values[char]
+    value = INTEGER_NUMERALS[char]
     total += value
     total -= 2 * prev_value if prev_value < value
     prev_value = value
